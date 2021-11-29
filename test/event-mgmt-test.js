f585c47f73db2916403b9cdf6d15c1cfa9320419
Ifeanyichukwu Welechi <welechi@icloud.com>
2021-11-29 12:20:34 +0100

    Update event-mgmt-test.js

--- test/event-mgmt-test.js
+++ test/event-mgmt-test.js
@@ -2,7 +2,7 @@ const Event = artifacts.require("Event");
 const truffleAssert = require("truffle-assertions")
 
 contract("Event", accounts => {
-    it("should prevent user from start or participate in new game if they have a currently active game. ", async() => {
+    it("should assert event roles.", async() => {
         let event = await Event.deployed()
         owner = await event.owner()
         assert.equal(owner, accounts[0])
